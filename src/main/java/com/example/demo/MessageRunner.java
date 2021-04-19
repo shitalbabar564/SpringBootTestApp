501a70a9399dc8f29c1d9a1d5d0ebbc2d8734244
HP <HP@DESKTOP-65EV0UU>
2021-04-19 11:32:26 +0530

    WH-8520-First Time File Update

---
+++ src/main/java/com/example/demo/MessageRunner.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to SpringBoot App..!");
+
+	}
+
+}
